9f02063ff6d38ce2ff54ea35644014630b950323
ssabrut <michael.gunawan2002@gmail.com>
2022-02-09 10:06:17 +0700

    Wishlist scaffolding

--- resources/views/livewire/wishlist.blade.php
+++ resources/views/livewire/wishlist.blade.php
@@ -1,5 +1,33 @@
 <div>
   <div class="mt-12">
-    <h1>Wishlist</h1>
+    <div>
+      <h1>Wishlist</h1>
+    </div>
+    <div>
+      <div class="flex flex-wrap">
+        @forelse ($wishlists as $wishlist)
+          <a class="mr-6" href="{{ route('products.show', ['product' => $wishlist->slug]) }}">
+            <div>
+              <img class="border border-gray-300 rounded-lg h-62 w-62 object-fill"
+                src="https://cdn.lumen.id/commerce/digimap/file/84417e65-4d7e-4762-86b9-87dbe7a75cf8/PDP-iPhone-13-Pink-1-medium.jpeg" />
+            </div>
+
+            <!-- card body -->
+            <div class="text-center mt-2">
+              <div>
+                <span class="text-gray-400">{{ $wishlist->category_name }}</span>
+              </div>
+              <div>
+                <span class="font-bold">{{ $wishlist->product_name }}</span>
+              </div>
+              <div>
+                <button type="button">Add to Cart</button>
+              </div>
+            </div>
+          </a>
+        @empty
+        @endforelse
+      </div>
+    </div>
   </div>
 </div>
